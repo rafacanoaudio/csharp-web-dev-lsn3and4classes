3671b32f691014ade5fd7184584963063a44ad10
rafacanoaudio <rafacanoaudio@gmail.com>
2020-10-13 07:26:31 -0500

    testing GH via VS

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,11 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Student rafa = new Student("Rafa", 1, 15, 4.0);
+            Student becca = new Student("Becca", 2, 85, 4.0);
+
+            Console.WriteLine(becca.GetGradeLevel());
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -11,5 +11,55 @@ namespace SchoolPractice
         private int StudentId;
         private int NumberOfCredits;
         private double Gpa;
+
+        public Student (string aName, int aStudentId, int aNumberOfCredits, double aGpa)
+        {
+            Name = aName;
+            StudentId = aStudentId;
+            NumberOfCredits = aNumberOfCredits;
+            Gpa = aGpa;
+        }
+
+        public Student (string aName, int aStudentId)
+        {
+            Name = aName;
+            StudentId = aStudentId;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public string GetGradeLevel()
+        {
+            if (NumberOfCredits < 30)
+            {
+                return ("Freshman");
+            } else if (NumberOfCredits < 60)
+            {
+                return ("Sophomore");
+            } else if (NumberOfCredits < 90)
+            {
+                return ("Junior");
+            } else
+            {
+                return ("Senior");
+            }
+        }
+
+        //public void AddGrade(int courseCredits, double grade)
+        //{
+
+
+
+        //    double totalQualityScore = qualityScore;
+
+
+        //    NumberOfCredits += courseCredits;
+        //    double qualityScore = grade * courseCredits;
+        //    double gpa = totalQualityScore / courseCredits;
+
+        //    double
+        //}
     }
+
+ 
 }
